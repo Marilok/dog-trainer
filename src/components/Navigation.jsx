77877eee343af2f212db9583936e99daf99ae7c6
Marilok <Marilok@users.noreply.github.com>
2021-07-03 10:47:24 +0200

    2.7.

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import logo from "../images/logo.png";
+import logoSmall from "../images/favicon.png";
 import { Link } from "gatsby";
 
 
@@ -8,8 +9,12 @@ const Navigation = (props) => {
     return (
         <nav className="nav">
       <Link to="/" style={{ height: "100%" }} className="nav__logo">
+        <picture>
+        <source media="(max-width: 800px)" srcSet={logoSmall}/>
         <img src={logo} className="nav__logo__img" alt="Logo" />
+        </picture>
       </Link>
+
       <div className="nav__links">
         {props.links.map((link) => {
           return (
@@ -21,13 +26,13 @@ const Navigation = (props) => {
       </div>
 
 
-      <div className="nav__lang">
+      {/* <div className="nav__lang">
         <select name="language" id="languages">
           {props.langs.map((lang) => {
             return <option value={lang.name} key={lang.name}>{lang.name}</option>;
           })}
         </select>
-      </div>
+      </div> */}
 
       
     </nav>
